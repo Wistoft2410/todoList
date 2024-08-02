4027dbb71c27a33176328dcd00b116cfed27c479
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-08-02 19:32:00 +0200

    Update App.js

--- todoList/App.js
+++ todoList/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -14,7 +13,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
