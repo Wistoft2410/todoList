af601b72eeb08ff3382083c5c344da55f568868d
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-08-02 19:50:17 +0200

    Added styles and title to App.js

--- todoList/App.js
+++ todoList/App.js
@@ -4,6 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
+      {/* Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        
+        <View styles={styles.items}>
+        {/* This is where the tasks will go! */}
+
+        </View>
+
+      </View>
       
     </View>
   );
@@ -12,7 +23,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20, 
+  }, 
+  sectionTitle: {
+    fontSize: 24, 
+    fontWeight: 'bold',
   },
 });
