ef9c23e7594994e10947d3bca010ba9cb1cecd69
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-08-04 20:39:59 +0200

    Update Task.js

--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -16,7 +16,7 @@ const Task = (props) => {
 
 const styles = StyleSheet.create({
     item: {
-        backkgroundColor: '#FFFFFF',
+        backgroundColor: '#FFFFFF',
         padding: 15,
         borderRadius: 10,
         flexDirection: 'row',
@@ -34,9 +34,19 @@ const styles = StyleSheet.create({
         height: 24, 
         backgroundColor: '#55BCF6',
         opdacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15, 
     },
-    itemText: {},
-    circular: {},
+    itemText: {
+        maxWidth: '80%',
+    },
+    circular: {
+        width: 12,
+        height: 12, 
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
 });
 
 export default Task; 
